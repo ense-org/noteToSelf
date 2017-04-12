61b859a205aae0440399a2f381efa9b3348a1771
jennykortina <jennykortina@gmail.com>
2017-04-12 17:31:01 -0400

    installed reactive native sound recorder

--- android/app/src/main/java/com/notetoself/MainApplication.java
+++ android/app/src/main/java/com/notetoself/MainApplication.java
@@ -3,6 +3,7 @@ package com.notetoself;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.horcrux.svg.SvgPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeAudioPackage(),
             new VectorIconsPackage(),
             new LottiePackage(),
             new SvgPackage(),
